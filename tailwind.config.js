974f343d85269abf7d32dd7214dd0de9a0a9dd9d
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2023-12-14 20:21:46 -0300

    ADD: animation logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        flowx: {
+          "0%": { transform: "skewY(3deg)" },
+          "25%": { transform: "skewY(0deg)" },
+          "50%": { transform: "skewX(3deg)" },
+          "75%": { transform: "skewX(0deg)" },
+          "100%": { transform: "skewY(3deg)" },
+        },
+      },
+      animation: {
+        "flow-x": "flowx 1s linear infinite"
+      },
+    },
   },
   plugins: [],
 }
